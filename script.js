f14b63e9ac01c7e42671ab1726d43f3636d82bd3
Yassine6262 <yassineaz2018@outlook.com>
2022-11-04 16:09:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function myfun()
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a+b
+
+}
+
+function myfun2() 
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a-b
+
+
+}
+function myfun3() 
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a*b
+
+
+}
+
+function myfun4() 
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a/b
+
+
+}
+
+if ( number < 0) {
+    greeting = "Kan niet";
+} else {
+
+}
